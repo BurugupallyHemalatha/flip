77e6e33188020036b9ccce29d3f9923f29f35e62
Hema Reddy <hemareddyr05@gmail.com>
2022-07-05 22:10:17 +0530

    commited

---
+++ src/app/Components/login/signup/signup.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+//there are three steps of creating reactive angular forms 
+/*
+1.define html form in the component form 
+2.define user model in component class 
+3.use the directtives provided by reactive forms module to associate model with view
+ */
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit {
+
+  constructor() { }
+  registrationForm =new FormGroup({
+    username:new FormControl('Hema'),
+    password:new FormControl(''),
+    confirmPassword: new FormControl(''),
+    address:new FormGroup({
+      city:new FormControl(''),
+      state:new FormControl(''),
+      postalCode:new FormControl('') 
+    })
+   });
+  ngOnInit(): void {
+  }
+
+}
